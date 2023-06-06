d4dc3a67eaf5859fae09b62255eb31799236fea8
Nephalio <erharteliott@gmail.com>
2023-06-06 18:09:52 +0200

    Correction de 2,3 erreurs + Ajout de chemins relatifs 06/06 @Néphalio

--- Optimisation_SESSAD.py
+++ Optimisation_SESSAD.py
@@ -1,14 +1,30 @@
 import numpy as np
+import os
 import pandas as pd
 
 # nécessite d'installer les librairies numpy ( pip install numpy ) et pandas ( pip install pandas )
 # chemin vers le fichier à modifier et peut nécessiter d'installer openyxl  ( pip install openpyxl )
 
-# Charger les fichier CSV en spécifiant que la première ligne est une ligne de données et non pas l'entête 
-centers = pd.read_csv(r"C:\Users\Lucas\Documents\UTBM\Cours\INFO2\IT45\Projet\instances\instances\30Missions-2centres\centers.csv" , header=None)         # mettre le bon format fichier + pip install openpyxl
-distances = pd.read_csv(r"C:\Users\Lucas\Documents\UTBM\Cours\INFO2\IT45\Projet\instances\instances\30Missions-2centres\distances.csv" , header=None)
-Employees = pd.read_csv(r"C:\Users\Lucas\Documents\UTBM\Cours\INFO2\IT45\Projet\instances\instances\30Missions-2centres\Employees.csv" , header=None)
-Missions = pd.read_csv(r"C:\Users\Lucas\Documents\UTBM\Cours\INFO2\IT45\Projet\instances\instances\30Missions-2centres\Missions.csv" , header=None)
+# Récupère le chemin du répertoire contenant le script Python
+script_directory = os.path.dirname(os.path.abspath(__file__))
+
+# Construit le chemin vers le dossier "instances"
+instances_directory = os.path.join(script_directory, "instances")
+
+# Construit le chemin vers le dossier "30Missions-2centres"
+missions_directory = os.path.join(instances_directory, "30Missions-2centres")
+
+# Construit les chemins vers les fichiers CSV
+centers_path = os.path.join(missions_directory, "centers.csv")
+distances_path = os.path.join(missions_directory, "distances.csv")
+employees_path = os.path.join(missions_directory, "Employees.csv")
+missions_path = os.path.join(missions_directory, "Missions.csv")
+
+# Charge les fichiers CSV
+centers = pd.read_csv(centers_path, header=None)
+distances = pd.read_csv(distances_path, header=None)
+employees = pd.read_csv(employees_path, header=None)
+missions = pd.read_csv(missions_path, header=None)
 
 
 # renommage des colonnes et affichage des données bruts
@@ -19,25 +35,25 @@ centers = centers.rename(columns={1: 'nom'})
 
 #print(f"distances : \n\n {distances} \n \n")
 
-Employees = Employees.rename(columns={0: 'id'})
-Employees = Employees.rename(columns={1: 'centre_ID'})
-Employees = Employees.rename(columns={2: 'compétence'})
-Employees = Employees.rename(columns={3: 'spécialité'})
+employees = employees.rename(columns={0: 'id'})
+employees = employees.rename(columns={1: 'centre_ID'})
+employees = employees.rename(columns={2: 'compétence'})
+employees = employees.rename(columns={3: 'spécialité'})
 #print(f"Employees : \n\n {Employees} \n \n")
 #print(Employees.columns)
 
-Missions = Missions.rename(columns={0: 'id'})
-Missions = Missions.rename(columns={1: 'jour'})
-Missions = Missions.rename(columns={2: 'heure_début'})
-Missions = Missions.rename(columns={3: 'heure_fin'})
-Missions = Missions.rename(columns={4: 'compétence'})
-Missions = Missions.rename(columns={5: 'spécialité'})
+missions = missions.rename(columns={0: 'id'})
+missions = missions.rename(columns={1: 'jour'})
+missions = missions.rename(columns={2: 'heure_début'})
+missions = missions.rename(columns={3: 'heure_fin'})
+missions = missions.rename(columns={4: 'compétence'})
+missions = missions.rename(columns={5: 'spécialité'})
 #print(f"Missions : \n\n {Missions} \n \n")
 
 
 
 #regroupe les employés avec leur centre pour calculer les compétence de chaque centre
-centers_Employees = pd.merge(centers, Employees, left_on='id', right_on='centre_ID', how='inner', suffixes=('_center' , '_employee')) # chaque employé est associé à son centre dans un nouveau tableau , jointure inner join sur centre_ID
+centers_Employees = pd.merge(centers, employees, left_on='id', right_on='centre_ID', how='inner', suffixes=('_center' , '_employee')) # chaque employé est associé à son centre dans un nouveau tableau , jointure inner join sur centre_ID
 centers_Employees = centers_Employees.drop('centre_ID', axis=1)     # la clé de jointure est supprimé car inutile
 print(centers_Employees)
 print("\n")
