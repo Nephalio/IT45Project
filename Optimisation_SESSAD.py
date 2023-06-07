185a3bb570c3621a2f056a6f09920ab2a4c99f34
Lucas-Te <lucas050602@gmail.com>
2023-06-07 11:15:47 +0200

    Update Optimisation_SESSAD.py
    Correction d'erreurs de nom de variables pour que l'affichage marche @lucas

--- Optimisation_SESSAD.py
+++ Optimisation_SESSAD.py
@@ -29,6 +29,11 @@ class Gene :            # chaque gène est une affectation d'une mission à un e
 #class Choromosome :     # représentation d'une solution , chaque solution est une liste d'affection et chaque affectation est une liste
     #def __init__(self,nb_affecation):   
         #for i in range(nb_affecation):
+
+class Population :      # population composé d'ensemble de solution
+    def __init__(self,donnees):
+        # crée une population initiale
+   
 '''
 
 class Donnees :     # classe qui recenses les données du problème
@@ -53,40 +58,41 @@ class Donnees :     # classe qui recenses les données du problème
         missions_path = os.path.join(missions_directory, "Missions.csv")
 
         # Charge les fichiers CSV
-        centers = pd.read_csv(centers_path, header=None)
-        distances = pd.read_csv(distances_path, header=None)
-        employees = pd.read_csv(employees_path, header=None)
-        missions = pd.read_csv(missions_path, header=None)
+        self.centers = pd.read_csv(centers_path, header=None)
+        self.distances = pd.read_csv(distances_path, header=None)
+        self.employees = pd.read_csv(employees_path, header=None)
+        self.missions = pd.read_csv(missions_path, header=None)
 
         # renommage des colonnes et affichage des données bruts
 
         self.centers = self.centers.rename(columns={0: 'id'})
         self.centers = self.centers.rename(columns={1: 'nom'})
-        #print(f"CENTRE : \n\n {centers} \n \n")
+        #print(f"CENTRE : \n\n {self.centers} \n \n")
 
-        #print(f"distances : \n\n {distances} \n \n")
+        #print(f"distances : \n\n {self.distances} \n \n")
 
-        employees = employees.rename(columns={0: 'id'})
-        employees = employees.rename(columns={1: 'centre_ID'})
-        employees = employees.rename(columns={2: 'compétence'})
-        employees = employees.rename(columns={3: 'spécialité'})
-        #print(f"Employees : \n\n {Employees} \n \n")
-        #print(Employees.columns)
+        self.employees = self.employees.rename(columns={0: 'id'})
+        self.employees = self.employees.rename(columns={1: 'centre_ID'})
+        self.employees = self.employees.rename(columns={2: 'compétence'})
+        self.employees = self.employees.rename(columns={3: 'spécialité'})
+        #print(f"Employees : \n\n {self.employees} \n \n")
+        #print(self.mployees.columns)
 
-        missions = missions.rename(columns={0: 'id'})
-        missions = missions.rename(columns={1: 'jour'})
-        missions = missions.rename(columns={2: 'heure_début'})
-        missions = missions.rename(columns={3: 'heure_fin'})
-        missions = missions.rename(columns={4: 'compétence'})
-        missions = missions.rename(columns={5: 'spécialité'})
-        #print(f"Missions : \n\n {Missions} \n \n")
+        self.missions = self.missions.rename(columns={0: 'id'})
+        self.missions = self.missions.rename(columns={1: 'jour'})
+        self.missions = self.missions.rename(columns={2: 'heure_début'})
+        self.missions = self.missions.rename(columns={3: 'heure_fin'})
+        self.missions = self.missions.rename(columns={4: 'compétence'})
+        self.missions = self.missions.rename(columns={5: 'spécialité'})
+        #print(f"Missions : \n\n {self.missions} \n \n")
 
 
+    def traitement_donnees(self):
 
         #regroupe les employés avec leur centre pour calculer les compétence de chaque centre
-        centers_Employees = pd.merge(centers, employees, left_on='id', right_on='centre_ID', how='inner', suffixes=('_center' , '_employee')) # chaque employé est associé à son centre dans un nouveau tableau , jointure inner join sur centre_ID
-        centers_Employees = centers_Employees.drop('centre_ID', axis=1)     # la clé de jointure est supprimé car inutile
-        print(centers_Employees)
+        self.centers_employees = pd.merge(self.centers, self.employees, left_on='id', right_on='centre_ID', how='inner', suffixes=('_center' , '_employee')) # chaque employé est associé à son centre dans un nouveau tableau , jointure inner join sur centre_ID
+        self.centers_employees = self.centers_employees.drop('centre_ID', axis=1)     # la clé de jointure est supprimé car inutile
+        print(self.centers_employees)
         print("\n")
 
         self.counts_competence = self.centers_employees.groupby(['id_center', 'nom'])['compétence'].value_counts().unstack(fill_value=0) # compte le nombre de compétence LSF et LPC de chaque centre
@@ -94,9 +100,15 @@ class Donnees :     # classe qui recenses les données du problème
         self.counts_specialite = self.centers_employees.groupby(['id_center', 'nom'])['spécialité'].value_counts().unstack(fill_value=0) # compte le nombre de spécialité de chaque centre
         print(print(f"Nombre de Spécialité de chaque centre : \n\n {self.counts_specialite} \n\n"))
 
-#print(centers_Employees.groupby(['compétence','nom']).value_counts())   # affiche qui possèdent des compétences LPC et LSF pour les centre
-#print(centers_Employees["compétence"].value_counts())               # compte le nombre d'employé ayant les compétences LSF et LPC total de tous les centres (sans distinction des centres)
-#print(centers_Employees.describe())
+        # quelques tests d'accès aux données des tableaux
+        print(f'test = {self.counts_specialite.iat[0,1]}')
+        print(f'test = {self.missions.iat[0,5]}')   #renvoie musique
+
+        #print(centers_Employees.groupby(['compétence','nom']).value_counts())   # affiche qui possèdent des compétences LPC et LSF pour les centre
+        #print(centers_Employees["compétence"].value_counts())               # compte le nombre d'employé ayant les compétences LSF et LPC total de tous les centres (sans distinction des centres)
+        #print(centers_Employees.describe())
+
+            
 
 
 
