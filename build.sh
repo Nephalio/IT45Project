0708c949bcad0e008fca63615575d5e55d49a3a4
Nephalio <erharteliott@gmail.com>
2023-06-16 22:07:36 +0200

    a

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Chemin vers l'interpréteur Python
+PYTHON="python3"
+
+# Commandes de compilation ou autres actions nécessaires
+$PYTHON -m pip install pandas openpyxl
+
+echo "Installation des dépendances terminée."
---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Chemin vers l'interpréteur Python
+PYTHON="python3"
+
+$PYTHON Optimisation_SESSAD.py
